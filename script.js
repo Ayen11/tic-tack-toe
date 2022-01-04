33306556719c9cb46ef46a15533de3e09ac2dde8
Ayen11 <einneuman@gmail.com>
2022-01-04 19:18:05 +0200

    got the basic layout to work

---
+++ script.js
@@ -0,0 +1,38 @@
+const gameboard = (() => {
+    
+    let boardArray = ["x","x","o","x","x","o","x","x","x","x"];
+    const boardRef = document.querySelector("#boardContainer");
+    
+    const makeBoard = () => {
+        for (let i = 1; i <= 9; i++) {
+        const inGridbtn = Button(i, boardRef);
+        console.log(inGridbtn.getIndex());
+    }
+};
+    
+    
+    
+    return {
+        makeBoard
+    };
+    
+
+
+})();
+
+
+const Button = (index, boardRef) => {
+    const getIndex = () => index;
+    let btnRef = document.createElement('button');
+        btnRef.classList.add('ButtonBoard');
+        boardRef.append(btnRef);
+
+    const setContent = (content) => {
+        btnRef.textContent = content;
+    };
+
+    return {setContent, getIndex}
+};
+ 
+ gameboard.makeBoard();
+ Button.getIndex;
